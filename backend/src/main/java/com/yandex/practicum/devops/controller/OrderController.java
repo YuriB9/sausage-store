6b3055cd456e23dc3f5e8e2b47e592b2539d07d2
Yuri Batkov <batckov.yury@yandex.ru>
2022-09-20 11:31:28 +0300

    fix bug

--- backend/src/main/java/com/yandex/practicum/devops/controller/OrderController.java
+++ backend/src/main/java/com/yandex/practicum/devops/controller/OrderController.java
@@ -80,7 +80,7 @@ public class OrderController {
           .collect(Collectors.toList());
 
         if (!CollectionUtils.isEmpty(list)) {
-            ttt new ResourceNotFoundException("Product not found");
+            throw new ResourceNotFoundException("Product not found");
         }
     }
 
