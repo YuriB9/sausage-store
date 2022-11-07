822ee322d734325c38b9ca5990ab02bd337f0eae
Yuri Batkov <batckov.yury@yandex.ru>
2022-11-07 12:17:54 +0300

    fix: update systend unit to user scope

--- backend/deploy.sh
+++ backend/deploy.sh
@@ -2,12 +2,12 @@
 #Если свалится одна из команд, рухнет и весь скрипт
 set -xe
 #Перезаливаем дескриптор сервиса на ВМ для деплоя
-sudo cp -rf sausage-store-backend.service /etc/systemd/system/sausage-store-backend.service
+sudo cp -rf sausage-store-backend-user.service /home/jarservice/.config/systemd/user/sausage-store-backend-user.service
 sudo rm -f /home/jarservice/sausage-store.jar||true
 #Переносим артефакт в нужную папку
 curl -u ${NEXUS_REPO_USER}:${NEXUS_REPO_PASS} -o sausage-store.jar ${NEXUS_REPO_URL}/repository/09-sausage-store-batkov-yuriy-backend/com/yandex/practicum/devops/sausage-store/${VERSION}/sausage-store-${VERSION}.jar
 sudo cp ./sausage-store.jar /home/jarservice/sausage-store.jar||true #"jar||true" говорит, если команда обвалится — продолжай
 #Обновляем конфиг systemd с помощью рестарта
-sudo systemctl daemon-reload
+systemctl --user daemon-reload
 #Перезапускаем сервис сосисочной
-sudo systemctl restart sausage-store-backend.service
+systemctl --user restart sausage-store-backend-user.service
