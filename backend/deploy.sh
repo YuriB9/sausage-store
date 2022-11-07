dc2d7e668582847e722a0176359d39571fc2109e
Yuri Batkov <batckov.yury@yandex.ru>
2022-11-07 12:56:47 +0300

    fix: add baselineOnMigrate, remove debug mesages

--- backend/deploy.sh
+++ backend/deploy.sh
@@ -4,9 +4,6 @@ set -xe
 #Перезаливаем дескриптор сервиса на ВМ для деплоя
 sudo cp -rf sausage-store-backend-user.service /home/jarservice/.config/systemd/user/sausage-store-backend-user.service
 sudo rm -f /home/jarservice/sausage-store.jar||true
-
-echo ${PSQL_USER}
-
 #Переносим артефакт в нужную папку
 curl -u ${NEXUS_REPO_USER}:${NEXUS_REPO_PASS} -o sausage-store.jar ${NEXUS_REPO_URL}/repository/09-sausage-store-batkov-yuriy-backend/com/yandex/practicum/devops/sausage-store/${VERSION}/sausage-store-${VERSION}.jar
 sudo cp ./sausage-store.jar /home/jarservice/sausage-store.jar||true #"jar||true" говорит, если команда обвалится — продолжай
