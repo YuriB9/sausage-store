1d167f7fc870071bdffeeee94f929765b6cc4a96
Yuri Batkov <batckov.yury@yandex.ru>
2022-11-29 11:11:43 +0300

    fix: ci registry url

--- backend/backend_deploy.sh
+++ backend/backend_deploy.sh
@@ -5,10 +5,9 @@ SPRING_DATASOURCE_URL=${SPRING_DATASOURCE_URL}
 SPRING_DATASOURCE_USERNAME=${SPRING_DATASOURCE_USERNAME}
 SPRING_DATASOURCE_PASSWORD=${SPRING_DATASOURCE_PASSWORD}
 SPRING_DATA_MONGODB_URI=${SPRING_DATA_MONGODB_URI}
-
 EOF
 docker network create -d bridge sausage_ci_network || true
-docker pull ${CI_REGISTRY_IMAGE}/sausage-backend:latest
+docker pull gitlab.praktikum-services.ru:5050/std-009-060/sausage-store/sausage-backend:latest
 docker container rm --force sausage-backend || true
 set -e
 docker container run --detach --name sausage-backend \
@@ -16,4 +15,4 @@ docker container run --detach --name sausage-backend \
     --restart always \
     --pull always \
     --env-file .env \
-    ${CI_REGISTRY_IMAGE}/sausage-backend:latest
+    gitlab.praktikum-services.ru:5050/std-009-060/sausage-store/sausage-backend:latest
