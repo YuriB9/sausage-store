cf18adbbd4dfbc1242333ee184a519e746dd9e8d
Yuri Batkov <batckov.yury@yandex.ru>
2022-12-01 13:03:46 +0300

    feat: update uri to yandex s3

--- backend/src/main/java/com/yandex/practicum/devops/SausageApplication.java
+++ backend/src/main/java/com/yandex/practicum/devops/SausageApplication.java
@@ -19,12 +19,12 @@ public class SausageApplication {
     @Bean
     CommandLineRunner runner(ProductService productService) {
         return args -> {
-            productService.save(new Product(1L, "Сливочная", 320.00, "https://res.cloudinary.com/sugrobov/image/upload/v1623323635/repos/sausages/6.jpg"));
-            productService.save(new Product(2L, "Особая", 179.00, "https://res.cloudinary.com/sugrobov/image/upload/v1623323635/repos/sausages/5.jpg"));
-            productService.save(new Product(3L, "Молочная", 225.00, "https://res.cloudinary.com/sugrobov/image/upload/v1623323635/repos/sausages/4.jpg"));
-            productService.save(new Product(4L, "Нюренбергская", 315.00, "https://res.cloudinary.com/sugrobov/image/upload/v1623323635/repos/sausages/3.jpg"));
-            productService.save(new Product(5L, "Мюнхенская", 330.00, "https://res.cloudinary.com/sugrobov/image/upload/v1623323635/repos/sausages/2.jpg"));
-            productService.save(new Product(6L, "Русская", 189.00, "https://res.cloudinary.com/sugrobov/image/upload/v1623323635/repos/sausages/1.jpg"));
+            productService.save(new Product(1L, "Сливочная", 320.00, "https://storage.yandexcloud.net/09-sausage-store-batkov-yuriy-images/6.jpg"));
+            productService.save(new Product(2L, "Особая", 179.00, "https://storage.yandexcloud.net/09-sausage-store-batkov-yuriy-images/5.jpg"));
+            productService.save(new Product(3L, "Молочная", 225.00, "https://storage.yandexcloud.net/09-sausage-store-batkov-yuriy-images/4.jpg"));
+            productService.save(new Product(4L, "Нюренбергская", 315.00, "https://storage.yandexcloud.net/09-sausage-store-batkov-yuriy-images/3.jpg"));
+            productService.save(new Product(5L, "Мюнхенская", 330.00, "https://storage.yandexcloud.net/09-sausage-store-batkov-yuriy-images/2.jpg"));
+            productService.save(new Product(6L, "Русская", 189.00, "https://storage.yandexcloud.net/09-sausage-store-batkov-yuriy-images/1.jpg"));
         };
     }
 }
