6ce5eb7b5ebb7b0b81fa277ec071af61ecbc7014
Yuri Batkov <batckov.yury@yandex.ru>
2022-12-27 13:37:39 +0000

    fix: add double sqr brackets

--- backend/backend_deploy_blue_green.sh
+++ backend/backend_deploy_blue_green.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set +e
 
-if [ $(docker container ps --filter="label=tk.batkovy.deployment=Blue" --quiet) ]
+if [[ $(docker container ps --filter="label=tk.batkovy.deployment=Blue" --quiet) ]]
 then
     NEW="green"
     OLD="blue"
@@ -13,31 +13,3 @@ fi
 echo "Starting "$NEW" backend."
 
 docker compose --file ~/docker-compose.yml --profile backend-only up --pull=always --detach --force-recreate backend-$NEW
-
-<<comment
-for i in {1..20}
-do
-    sleep 1
-
-    HEALTH=$(docker inspect --format "{{json .State.Health.Status }}" student-backend-1)
-
-    if [[ ${HEALTH} == "healthy"]]
-    then
-        echo "New '$NEW' service seems OK."
-        sleep 2  # Ensure all requests were processed
-
-        echo "Stopping "$OLD" backend"
-        docker compose --file ~/docker-compose.yml --profile backend-only up --pull=always --detach --force-recreate backend-$OLD
-
-        echo "Deployment successful!"
-        return 0
-    fi
-
-    echo "New '$NEW' service is not ready yet. Waiting ($i)..."
-
-done
-
-echo "New '$NEW' service did not raise, killing it. Failed to deploy T_T"
-#docker rm -f $NEW
-return 5
-comment
